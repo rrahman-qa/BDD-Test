54d1b6e2dae97b4245a23970d82a931a307d855c
Rahi Rahman <rrahman.qa@gmail.com>
2018-09-08 15:19:56 +0100

    modified runner to include plugin for json format cucumber results

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features={"src/test/resources"},
-        plugin = { "pretty", "html:target/htmlreports" },
+        plugin = { "pretty", "html:target/htmlreports", "json:target/cucumber.json" },
         glue = "steps"
 )
 public class CucumberRunnerTest {
